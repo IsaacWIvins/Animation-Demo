59ae75bab4302f7c92d7382325b85a97285804d0
Isaac Ivins <isaac@magicdevelopment.com>
2019-01-28 21:42:27 -0500

    asfd

---
+++ build/precache-manifest.854bb42e6806d28649fec242948a9b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72382d627549057aa764",
+    "url": "/Animation-Demo/static/css/main.03737c17.chunk.css"
+  },
+  {
+    "revision": "72382d627549057aa764",
+    "url": "/Animation-Demo/static/js/main.72382d62.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Animation-Demo/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "10634375c529a1bb652d",
+    "url": "/Animation-Demo/static/js/runtime~main.10634375.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Animation-Demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cfb7bef0b0fba1813a461e491525cc93",
+    "url": "/Animation-Demo/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ChatBubbles from './Animations/chatBubbles.js';
+import { TweenLite, Expo, TimelineLite, Power1} from 'gsap';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.timeLine = new TimelineLite({ paused: true })
+  }
+
+  getStarted = () => {
+
+    // fade-out Get-Started-Container and change background gradient
+    this.timeLine.to(this.background, 3, {
+      backgroundImage: "linear-gradient(to right top, #051937, #004870, #007d9f, #00b6bc, #24eec4)"
+    }, 0).to(this.getStartedContainer, 0.75, {
+      opacity: 0,
+      ease: Power1.easeInOut,
+      display: 'none',
+    }, 0)
+
+    // fade in Content-Container
+    this.timeLine.to(this.contentRef, 1, {
+      opacity: 1,
+      display: 'flex',
+      ease: Power1.easeInOut
+    }, .75)
+
+    // play timeline
+    this.timeLine.play()
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div className="App" ref={div => this.background = div}>
+
+        <div className="Get-Started-Container" ref={div => this.getStartedContainer = div}>
+          <div onClick={this.getStarted}>
+            <h1 style={{color: 'white'}}>Get Started</h1>
+          </div>
+        </div>
+
+        <div className="Content-Container" ref={div => this.contentRef = div}>
+          <div className="Left-Box" ref={div => this.leftBox = div}>
+            <h1>left</h1>
+          </div>
+          <div className="Right-Box" ref={div => this.rightBox = div}>
+            <h1>right</h1>
+          </div>
+        </div>
+
       </div>
     );
   }
